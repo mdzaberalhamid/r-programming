99f4d19613c54b2188f217079ef2341db94a7ad5
Md. Zaber Al Hamid <86970972+mdzaberalhamid@users.noreply.github.com>
2025-10-06 23:51:06 +0300

    Create 04_Concatenate.R

---
+++ 04_Concatenate.R
@@ -0,0 +1,13 @@
+# Example 1
+text <- "Awesome"
+paste("R is", text)
+
+#Example 2
+text1 <- "I am learning"
+text2 <- "R programming!"
+paste(text1, text2)
+
+#Example 3
+num1 <- 1
+num2 <- 9
+paste(num1 + num2)
