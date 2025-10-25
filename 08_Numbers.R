1bd5772b915847334e6053ff73b4e4e35f5a8fa6
Md. Zaber Al Hamid <86970972+mdzaberalhamid@users.noreply.github.com>
2025-10-26 00:05:50 +0300

    Add 08_Numbers.R

---
+++ 08_Numbers.R
@@ -0,0 +1,35 @@
+# Numeric
+
+n1 <- 10.5
+print(n1)
+class(n1)
+
+n2 <- 9
+print(n2)
+class(n2)
+
+# Integer
+
+i1 <- 5L
+print(i1)
+class(i1)
+
+# Complex
+
+c1 <- 3 + 2i
+print(c1)
+class(c1)
+
+# Type conversion
+
+t1 <- 10.999
+x <- as.integer(t1)
+print(x)
+class(x)
+
+t2 <- 3L
+class(t2)
+
+y <- as.numeric(t2)
+print(y)
+class(y)
