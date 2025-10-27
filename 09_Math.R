df9c024b0e832a435b50607b67480a39f0912263
Md. Zaber Al Hamid <86970972+mdzaberalhamid@users.noreply.github.com>
2025-10-27 20:53:22 +0200

    basic math operations and functions in 09_Math.R

---
+++ 09_Math.R
@@ -0,0 +1,19 @@
+# Basic
+
+print(2 + 3)
+
+print(10 - 1)
+
+# Built-in math functions
+
+print(max(5, 10, 15))
+
+print(min(1,2,3))
+
+print(sqrt(16))
+
+print(abs(-2.345))
+
+print(ceiling(1.4))
+
+print(floor(1.4))
