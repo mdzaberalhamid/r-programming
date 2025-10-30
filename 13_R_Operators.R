085057c37ed7809cf72e29126aa373aa27ad2368
Md. Zaber Al Hamid <86970972+mdzaberalhamid@users.noreply.github.com>
2025-10-30 21:15:25 +0200

    R Operators

---
+++ 13_R_Operators.R
@@ -0,0 +1,27 @@
+a <- 10
+b <- 3
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a ^ b)
+print(a %% b)
+
+9 -> c
+print(c)
+
+print(a == b)
+print(a != b)
+print(a > b)
+print(a < b)
+print(a <= 10)
+print(a >= b)
+
+print(!TRUE)
+print(a == 10 & b == 3)   # TRUE
+print(a == 10 | b == 1)   # TRUE
+print(a == 0 | b == 1)   # FALSE
+
+x <- 1:10
+print(x)
