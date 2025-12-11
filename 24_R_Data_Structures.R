9ea88b0be3b841e3f912093c19b8a2d0199b1078
Md. Zaber Al Hamid <86970972+mdzaberalhamid@users.noreply.github.com>
2025-12-11 21:14:38 +0200

    Update 24_R_Data_Structures.R

--- 24_R_Data_Structures.R
+++ 24_R_Data_Structures.R
@@ -7,3 +7,23 @@ print(cars)
 # Lists
 list1 <- list('Apple', 9, TRUE)
 print(list1)
+
+# Matrices
+matrix1 <- matrix(c(1,2,3,4,5,6), nrow=3, ncol=2)
+print(matrix1)
+
+# Arrays
+one_dimensional_array <- c(1:24)
+print(one_dimensional_array)
+
+multi_dimensional_array <- array(one_dimensional_array, dim = c(4,3,2))
+print(multi_dimensional_array)
+
+# Data Frames
+Data_Frame <- data.frame(
+  Training <- c("Strength", "Stamina", "Other"),
+  Pulse <- c(100, 150, 120),
+  Duration <- c(60, 30, 45)
+)
+
+print(Data_Frame)
