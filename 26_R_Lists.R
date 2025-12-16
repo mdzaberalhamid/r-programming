6affccb2130ea0f5beadead76f1f88f13903d510
Md. Zaber Al Hamid <86970972+mdzaberalhamid@users.noreply.github.com>
2025-12-16 23:16:49 +0200

    R Lists

---
+++ 26_R_Lists.R
@@ -0,0 +1,15 @@
+# R_Lists
+
+# Example 1
+list1 <- list('apple','banana','mango')
+print(list1)
+
+# Accessing a list item
+print(list1[1])
+
+# Changing a list item
+list1[1] <- 'cherry'  
+print(list1)
+
+# List length
+print(length(list1)) # 3
